f5a3925be7fe0ff05bb51d06f6fddbf75d9435d9
shawen17 <shawen022@yahoo.com>
2024-06-11 20:43:53 -0400

    user page

--- lendsqr/src/components/Users.jsx
+++ lendsqr/src/components/Users.jsx
@@ -8,7 +8,6 @@ import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 import { StatusUpdate } from "./utility/AdminAction";
 import axios from "axios";
-// import { mergeFields } from "./utility/AdminAction";
 import { UserContext } from "./ContextManager";
 
 const Users = (props) => {
@@ -17,59 +16,10 @@ const Users = (props) => {
   const [submenu, setSubmenu] = useState(false);
   const [clicked, setClicked] = useState(false);
   const [inputs, setInputs] = useState({});
-  //   const [filtered_result, setResult] = useState( {  items: {
-  //     users_paginated: [],
-  //     all_users: 0,
-  //     active: 0,
-  //     loan: 0,
-  //     savings: 0,
-  //   },
-  // });
   const [filtered, setFiltered] = useState(0);
   const [position, setPosition] = useState({ top: 0, left: 0 });
   const navigate = useNavigate();
-
   const result = useContext(UserContext);
-  // if (clicked && filtered) {
-  //   result = filtered_result.items;
-  // }
-
-  // useEffect(() => {
-  //   setResult(props.currentData);
-  // }, [props.currentData]);
-
-  // useEffect(() => {
-  //   if (clicked && filtered) {
-  //     const profileKeys = ["userName", "status", "email", "phoneNumber"];
-  //     const organizationKeys = ["orgName"];
-  //     const profile = mergeFields(inputs, profileKeys);
-  //     const organization = mergeFields(inputs, organizationKeys);
-
-  //     const config = {
-  //       headers: {
-  //         "Content-Type": "multipart/form-data",
-  //         Accept: "application/json",
-  //       },
-  //     };
-  //     axios
-  //       .get(
-  //         `${process.env.REACT_APP_LENDSQR_API_URL}/api/advance-filter?page=${props.page}`,
-  //         {
-  //           params: {
-  //             profile: JSON.stringify({ profile }),
-  //             organization: JSON.stringify({ organization }),
-  //           },
-  //         },
-  //         config
-  //       )
-  //       .then((response) => {
-  //         setResult(response.data);
-  //       })
-  //       .catch((error) => {
-  //         console.error(error);
-  //       });
-  //   }
-  // }, [filtered, inputs, clicked, props.page]);
 
   const handleChange = (event) => {
     const name = event.target.name;
