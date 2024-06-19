7c2da6d154e2126f546106a0040eb09428e1ffbb
shawen17 <shawen022@yahoo.com>
2024-06-19 00:00:15 -0400

    pytest

--- lendsqr_backend/lendsqr/tests/views_load_users_tests.py
+++ lendsqr_backend/lendsqr/tests/views_load_users_tests.py
@@ -1,40 +1,40 @@
-import pytest
-from django.contrib.auth import get_user_model
+# import pytest
+# from django.contrib.auth import get_user_model
 
-User = get_user_model()
+# User = get_user_model()
 
 
-@pytest.mark.parametrize(
-    "page, search,validity",
-    [
-        (1, "Beaulah0", 1),
-        (2, "", 1),
-    ],
-)
-@pytest.mark.fast
-@pytest.mark.django_db
-def test_load_user(client, page, search, validity):
-    email = "shawn@gmail.com"
-    password = "admin"
+# @pytest.mark.parametrize(
+#     "page, search,validity",
+#     [
+#         (1, "beau", 1),
+#         (2, "", 1),
+#     ],
+# )
+# @pytest.mark.fast
+# @pytest.mark.django_db
+# def test_load_user(client, page, search, validity):
+#     email = "shawn@gmail.com"
+#     password = "admin"
 
-    User.objects.create_user(
-        first_name="shawn",
-        last_name="shawn",
-        email=email,
-        password=password,
-    )
-    login_url = "/auth/jwt/create/"
-    data = {"email": email, "password": password}
-    # client = APIClient()
-    print("========login initiated===========")
-    response = client.post(login_url, data, format="json")
-    token = response.data["access"]
+#     User.objects.create_user(
+#         first_name="shawn",
+#         last_name="shawn",
+#         email=email,
+#         password=password,
+#     )
+#     login_url = "/auth/jwt/create/"
+#     data = {"email": email, "password": password}
+#     # client = APIClient()
+#     print("========login initiated===========")
+#     response = client.post(login_url, data, format="json")
+#     token = response.data["access"]
 
-    print("========login successful===========")
-    url = f"/api/users?page={page}&search={search}"
+#     print("========login successful===========")
+#     url = f"/api/users?page={page}&search={search}"
 
-    headers = {"content_type": "application/json", "Authorization": token}
-    print("========fetching users===========")
-    response = client.get(url, headers=headers)
-    assert len(response.data["users_paginated"]) >= validity
-    print("========users loaded successfully===========")
+#     headers = {"content_type": "application/json", "Authorization": token}
+#     print("========fetching users===========")
+#     response = client.get(url, headers=headers)
+#     assert len(response.data["users_paginated"]) >= validity
+#     print("========users loaded successfully===========")
