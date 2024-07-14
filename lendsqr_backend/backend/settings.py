c7c5d8386ad82079e3e734ad18afa4462b2e1217
shawen17 <shawen022@yahoo.com>
2024-07-14 18:35:47 -0400

    settings

--- lendsqr_backend/backend/settings.py
+++ lendsqr_backend/backend/settings.py
@@ -227,7 +227,5 @@ CORS_ORIGIN_WHITELIST = [
 
 AUTH_USER_MODEL = "lendsqr.User"
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
