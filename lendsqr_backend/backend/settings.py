b9f2d8040f77fd9ca14400f0250b4bd72e1068b9
shawen17 <shawen022@yahoo.com>
2024-09-06 22:32:58 -0400

    sqlite deleted

--- lendsqr_backend/backend/settings.py
+++ lendsqr_backend/backend/settings.py
@@ -96,12 +96,6 @@ TEMPLATES = [
 WSGI_APPLICATION = "backend.wsgi.application"
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 
 # DATABASES = {
